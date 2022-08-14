30278f2e50fdb435a293af869083eab520981181
shu <st.iii9wh@gmail.com>
2022-08-14 05:28:55 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,20 @@
+def FizzBuzz(num)
+ 
+    if num % 3 == 0 && num % 5 == 0
+      return "FizzBuzz"
+    elsif num % 5 == 0
+      return "Buzz"
+    elsif num % 3 == 0 
+      return "Fizz"
+    else
+      return num
+    end
+  
+end
+
+num_max = 100
+
+(1..num_max).each do |num|
+  puts FizzBuzz(num)
+end
+
